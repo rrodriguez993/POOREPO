2b3a06cee754b422cbe3d9d4f08c4ae0140d994b
rrodriguez993 <juniorlarios25@gmail.com>
2026-09-22 22:14:12 -0600

    Agregado ejercicio#1

---
+++ Ejercicio#1/Ejercicio#1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class Empleado
+{
+    public string Nombre { get; set; }
+    public decimal SalarioBase { get; set; }
+
+    public Empleado(string nombre, decimal salarioBase)
+    {
+        Nombre = nombre;
+        SalarioBase = salarioBase;
+    }
+
+    public abstract decimal CalcularSalarioTotal();
+}
+
+class Desarrollador : Empleado
+{
+    public int HorasExtra { get; set; }
+
+    public Desarrollador(string nombre, decimal salarioBase, int horasExtra)
+        : base(nombre, salarioBase)
+    {
+        HorasExtra = horasExtra;
+    }
+
+    public override decimal CalcularSalarioTotal()
+    {
+        return SalarioBase + (HorasExtra * 10);
+    }
+}
+
+class Gerente : Empleado
+{
+    public decimal Bono { get; set; }
+
+    public Gerente(string nombre, decimal salarioBase, decimal bono)
+        : base(nombre, salarioBase)
+    {
+        Bono = bono;
+    }
+
+    public override decimal CalcularSalarioTotal()
+    {
+        return SalarioBase + Bono;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Empleado> empleados = new List<Empleado>()
+        {
+            new Desarrollador("Juan",3000,100),
+            new Gerente("Maria",5000,1200)
+        };
+
+        foreach (Empleado e in empleados)
+        {
+            Console.WriteLine($"{e.Nombre}: Q{e.CalcularSalarioTotal()}");
+        }
+    }
+}
