ccc285a34e013a371441a8f9b18247cb6d8d239b
rrodriguez993 <juniorlarios25@gmail.com>
2026-09-22 22:17:17 -0600

    Agregado ejercicio 5

---
+++ Ejercicio#5/Ejercicio#5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class CuentaBancaria
+{
+    public decimal Saldo { get; set; }
+
+    public CuentaBancaria(decimal saldo)
+    {
+        Saldo = saldo;
+    }
+
+    public abstract decimal CalcularInteres();
+}
+
+class CuentaAhorro : CuentaBancaria
+{
+    public CuentaAhorro(decimal saldo) : base(saldo) { }
+
+    public override decimal CalcularInteres()
+    {
+        return Saldo * 0.03m;
+    }
+}
+
+class CuentaInversion : CuentaBancaria
+{
+    public CuentaInversion(decimal saldo) : base(saldo) { }
+
+    public override decimal CalcularInteres()
+    {
+        return Saldo * 0.08m;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<CuentaBancaria> cuentas = new List<CuentaBancaria>()
+        {
+            new CuentaAhorro(10000),
+            new CuentaInversion(10000)
+        };
+
+        foreach (var c in cuentas)
+        {
+            Console.WriteLine($"Interés generado: Q{c.CalcularInteres()}");
+        }
+    }
+}
