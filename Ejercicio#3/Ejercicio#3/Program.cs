d605dbc9917f780e060a5941a04a479f5668515c
rrodriguez993 <juniorlarios25@gmail.com>
2026-09-22 22:16:22 -0600

    Agregados ejercicio 3 y 4

---
+++ Ejercicio#3/Ejercicio#3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class Vehiculo
+{
+    public double Combustible { get; set; }
+
+    public Vehiculo(double combustible)
+    {
+        Combustible = combustible;
+    }
+
+    public abstract double CalcularAutonomia();
+}
+
+class Automovil : Vehiculo
+{
+    public Automovil(double combustible) : base(combustible) { }
+
+    public override double CalcularAutonomia()
+    {
+        return Combustible * 15;
+    }
+}
+
+class Camion : Vehiculo
+{
+    public Camion(double combustible) : base(combustible) { }
+
+    public override double CalcularAutonomia()
+    {
+        return Combustible * 5;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Vehiculo> flota = new List<Vehiculo>()
+        {
+            new Automovil(40),
+            new Camion(40)
+        };
+
+        foreach (Vehiculo v in flota)
+        {
+            Console.WriteLine($"Autonomía: {v.CalcularAutonomia()} km");
+        }
+    }
+}
---
+++ Ejercicio#4/Ejercicio#4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class Notificacion
+{
+    public abstract void Enviar(string mensaje);
+}
+
+class CorreoElectronico : Notificacion
+{
+    public override void Enviar(string mensaje)
+    {
+        Console.WriteLine($"Correo enviado: {mensaje}");
+    }
+}
+
+class SMS : Notificacion
+{
+    public override void Enviar(string mensaje)
+    {
+        Console.WriteLine($"SMS enviado: {mensaje}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Notificacion> notificaciones = new List<Notificacion>()
+        {
+            new CorreoElectronico(),
+            new SMS()
+        };
+
+        foreach (var n in notificaciones)
+        {
+            n.Enviar("Alerta crítica del servidor");
+        }
+    }
+}
